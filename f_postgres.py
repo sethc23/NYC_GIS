262431520a892a539533ba90a5f33b3e10ce5cd1
sethc23 <sethc23@users.noreply.github.com>
2015-09-12 17:06:23 -0400

    added lua longest common subsequence (LCS) function (ripped from rosettacode.org; will add comment with source attribute

--- f_postgres.py
+++ f_postgres.py
@@ -1811,6 +1811,39 @@ class pgSQL_Functions:
             """
             self.T.to_sql(                      cmd)
 
+        def z_str_comp_lcs(self):
+            cmd="""
+                DROP FUNCTION IF EXISTS         z_str_comp_lcs( text,text);
+                CREATE OR REPLACE FUNCTION      z_str_comp_lcs( s1              text,
+                                                                s2              text)
+                RETURNS                         text
+                AS $BODY$
+
+
+                function LCS ( a, b )
+                    if #a == 0 or #b == 0 then
+                        return                  ""
+                    elseif a:sub( -1, -1 ) == b:sub( -1, -1 ) then
+                        return                  LCS( a:sub( 1, -2 ), b:sub( 1, -2 ) ) .. a:sub( -1, -1 )
+                    else
+                        local a_sub         =   LCS( a, b:sub( 1, -2 ) )
+                        local b_sub         =   LCS( a:sub( 1, -2 ), b )
+
+                        if #a_sub > #b_sub then
+                            return              a_sub
+                        else
+                            return              b_sub
+                        end
+                    end
+                end
+
+                local res                   =   LCS(s1,s2)
+                return res
+
+                $BODY$ LANGUAGE plluau;
+            """
+            self.T.to_sql(                      cmd)
+
         def z_update_with_geom_from_coords(self):
             """
 
