122ddb721d955184b8f415345c048da514f30b5c
sethc23 <sethc23@users.noreply.github.com>
2015-09-21 14:16:37 -0400

    further debugging- set break condition for while loop when all search permutations are evaluated

--- tmp.py
+++ tmp.py
@@ -12,6 +12,9 @@ def do_this(plpy,iter_round,DEBUG=False,EMBED=False):
     # import IPython as I
     # I.embed_kernel()
 
+    # import ipdb
+    # ipdb.set_trace()
+
     if DEBUG:
         os.system("echo '\\n\\n'`date --utc`")
 
@@ -45,117 +48,137 @@ def do_this(plpy,iter_round,DEBUG=False,EMBED=False):
                 """
 
         _updated = False
-
         ra = plpy.execute( qry_a % {'concat':a_concat,'cond':a_str_idx_cond} )
         if not ra:
             return _updated,0
-
+        # else...
         for r in ra:
-            if DEBUG:
-                log_to_file('----------ts_uid: %(a_idx)s' % r)
 
-            if EMBED:
-                if '%(a_idx)s' % r=='385':
-                    import ipdb
-                    ipdb.set_trace()
-
-            for k,v in r.iteritems():
-
-                # FIRST
-                # check if even number of single-quotes
-                if str(v).count("'") % 2==0:
-                    pass
-
-                # HACK -- replace right-most single-quote with 2 single-quotes
-                else:
-                    pt = v.rfind("'")
-                    r[k] = v[:pt] + "'" + v[pt:]
-
-                # SECOND
-                # since r is passed through 2 layers of substitution:
-                #   (1) first when creating qry variable, and
-                #   (2) within function z_string_matching called by qry
-                # then:
-                #   "'" --> "''''"
-                if str(v).count("'"):
-                    r[k] = re.sub(r"'{1}","''''",v)
-
-            _t = {
-                'f_qry_a':qry_a.replace("'","''")
-                      % {'concat':a_concat.replace("'","''").replace('\\','\\\\'),
-                         'cond':a_str_cond.replace("'","''") }
-                            % r,
-
-                'f_qry_b':qry_b.replace("'","''")
-                      % {'concat':b_concat.replace("'","''").replace('\\','\\\\'),
-                         'cond':b_str_cond.replace("'","''") }
-                            % r,
-
-                'res_cond':result_cond
-                 }
-
-            qry =   """
-                    WITH qry AS (
-                        SELECT (z).*
-                        FROM z_string_matching(
-                            e'%(f_qry_a)s'::text,
-                            e'%(f_qry_b)s'::text) z
-                    )
-                    ,str_matching_form as (
-                        select
-                            _str.ts_uid,_str.ts_div_line,_str.ts_station
-                            ,z.a_str ts_str,
-                                z.jaro_score::DOUBLE PRECISION,
-                                z.b_str match_str
-                            ,_stat.station_name sub_station,
-                                div_line sub_div_line,
-                                z.b_idx::INTEGER sub_idx
-                            ,z.other_matches
-                        FROM qry z
-                        INNER JOIN str_matching _str
-                        ON _str.ts_uid = z.a_idx::INTEGER
-                        INNER JOIN sub_stations _stat
-                        ON _stat.uid = z.b_idx::INTEGER
-                        %(res_cond)s
-                    )
-                    ,upd AS (
-                        UPDATE str_matching _str
-                        SET
-                            ts_str=_res.ts_str,
-                            jaro_score=_res.jaro_score,
-                            match_str=_res.match_str,
-                            sub_station=_res.sub_station,
-                            sub_div_line=_res.sub_div_line,
-                            sub_idx=_res.sub_idx
-                        FROM  str_matching_form _res
-                        WHERE _res.ts_uid=_str.ts_uid
-                        AND _res.jaro_score > _str.jaro_score
-                        RETURNING uid
-                    )
-                    SELECT * FROM upd
-                    """ % _t
-
-            # log_to_file(qry)
-            q_res = plpy.execute(qry)
-
-            if q_res:
-                log_to_file("found")
-                _updated = True
-
-            if DEBUG:
-                # log_to_file(qry)
-                log_to_file(q_res)
+            if a_updates.count(r["a_idx"]):
                 pass
 
-            break
+            else:
+
+                if DEBUG:
+                    log_to_file('----------ts_uid: %(a_idx)s' % r)
+
+                for k,v in r.iteritems():
+
+                    # FIRST
+                    # check if even number of single-quotes
+                    if str(v).count("'") % 2==0:
+                        pass
+
+                    # HACK -- replace right-most single-quote with 2 single-quotes
+                    else:
+                        pt = v.rfind("'")
+                        r[k] = v[:pt] + "'" + v[pt:]
+
+                    # SECOND
+                    # since r is passed through 2 layers of substitution:
+                    #   (1) first when creating qry variable, and
+                    #   (2) within function z_string_matching called by qry
+                    # then:
+                    #   "'" --> "''''"
+                    if str(v).count("'"):
+                        r[k] = re.sub(r"'{1}","''''",v)
+
+                _t = {
+                    'f_qry_a':qry_a.replace("'","''")
+                          % {'concat':a_concat.replace("'","''").replace('\\','\\\\'),
+                             'cond':a_str_cond.replace("'","''") }
+                                % r,
+
+                    'f_qry_b':qry_b.replace("'","''")
+                          % {'concat':b_concat.replace("'","''").replace('\\','\\\\'),
+                             'cond':b_str_cond.replace("'","''") }
+                                % r,
+
+                    'res_cond':result_cond
+                     }
+
+                qry =   """
+                        WITH qry AS (
+                            SELECT (z).*
+                            FROM z_string_matching(
+                                e'%(f_qry_a)s'::text,
+                                e'%(f_qry_b)s'::text) z
+                        )
+                        ,str_matching_form as (
+                            select
+                                _str.ts_uid,_str.ts_div_line,_str.ts_station
+                                ,z.a_str ts_str,
+                                    z.jaro_score::DOUBLE PRECISION,
+                                    z.b_str match_str
+                                ,_stat.station_name sub_station,
+                                    div_line sub_div_line,
+                                    z.b_idx::INTEGER sub_idx
+                                ,z.other_matches
+                            FROM qry z
+                            INNER JOIN str_matching _str
+                            ON _str.ts_uid = z.a_idx::INTEGER
+                            INNER JOIN sub_stations _stat
+                            ON _stat.uid = z.b_idx::INTEGER
+                            %(res_cond)s
+                        )
+                        ,upd AS (
+                            UPDATE str_matching _str
+                            SET
+                                ts_str=_res.ts_str,
+                                jaro_score=_res.jaro_score,
+                                match_str=_res.match_str,
+                                sub_station=_res.sub_station,
+                                sub_div_line=_res.sub_div_line,
+                                sub_idx=_res.sub_idx
+                            FROM  str_matching_form _res
+                            WHERE _res.ts_uid=_str.ts_uid
+                            AND _res.jaro_score > _str.jaro_score
+                            RETURNING uid
+                        )
+                        SELECT * FROM str_matching_form
+                        """ % _t
+
+                q_res = plpy.execute(qry)
+
+                if q_res:
+                    if DEBUG:
+                        log_to_file("found")
+                        log_to_file(q_res)
+
+                    _updated = True
+
+                if DEBUG:
+                    # log_to_file(qry)
+                    log_to_file(q_res)
+                    pass
+
+                break
 
         return _updated,r
 
     def mark_unmatched(r):
-        plpy.execute("UPDATE str_matching SET jaro_score=-1 WHERE ts_uid=%(a_idx)s"%r)
+        qry = """    WITH upd AS (
+                                SELECT uid
+                                FROM str_matching
+                                WHERE ts_uid=%(a_idx)s
+                                AND jaro_score=0
+                            )
+                            UPDATE str_matching s
+                            SET jaro_score=-1
+                            FROM upd u
+                            WHERE s.uid=u.uid;
+              """ % r
         if DEBUG:
-            log_to_file('marked')
+            log_to_file('marking')
             log_to_file(r)
+
+        # TODO: remove
+        # if EMBED and r['a_idx']==373:
+        #     import ipdb
+        #     ipdb.set_trace()
+
+        plpy.execute(qry)
+
 
     def run_iter():
 
@@ -178,7 +201,8 @@ def do_this(plpy,iter_round,DEBUG=False,EMBED=False):
         #   each substitution interprets "\\" as "\"
         #   final log print of query before execution needs "\\\\" to read "\\"
 
-        global a_concat,b_concat
+        global a_concat,b_concat,a_updates
+        a_updates = []
         end = False
         while True:
 
@@ -195,7 +219,12 @@ def do_this(plpy,iter_round,DEBUG=False,EMBED=False):
                     res,r = run_query(**_t)
                     if res:
                         updated = True
+                        a_updates.append(r['a_idx'])
                     elif not r:
+                        end = True
+                        break
+                    # cut loop when everything already updated
+                    elif not res and a_updates.count(r['a_idx']):
                         end = True
                         break
 
@@ -204,23 +233,15 @@ def do_this(plpy,iter_round,DEBUG=False,EMBED=False):
                     elif (not updated
                         and i==len(a_str_concat)-1
                         and j==len(b_str_concat)-1):
-                            log_to_file('MARKING UNMATCHED')
+                            a_updates.append(r['a_idx'])
+                            if DEBUG:
+                                log_to_file('MARKING UNMATCHED')
                             mark_unmatched(r)
 
                 if end:
                     break
             if end:
                 break
-
-
-
-
-
-        #         if DEBUG:
-        #             break
-        #     if DEBUG:
-        #         break
-
 
     a=0
 
@@ -257,8 +278,6 @@ def do_this(plpy,iter_round,DEBUG=False,EMBED=False):
         run_iter()
 
     elif iter_round=='second':
-        #import IPython as I
-        #I.embed_kernel()
         res = plpy.execute("""WITH old AS (SELECT uid FROM str_matching WHERE jaro_score=-1) UPDATE str_matching s SET jaro_score=0 FROM old o WHERE o.uid = s.uid;""")
         a_prefix,a_suffix = '',''
         b_prefix,b_suffix = '',''
@@ -279,14 +298,27 @@ def do_this(plpy,iter_round,DEBUG=False,EMBED=False):
                             # _t['line'],
                            ]
 
-        result_cond = ' WHERE z.jaro_score::DOUBLE PRECISION > _str.jaro_score'
+        result_cond = ' WHERE z.jaro_score::DOUBLE PRECISION >= _str.jaro_score'
 
         for it in b_str_conditions:
+            if DEBUG:
+                log_to_file('cond #: %s' % b_str_conditions.index(it))
+
             b_str_cond = ' WHERE ' + it
             if EMBED:
                 import IPython as I
                 I.embed_kernel()
             run_iter()
+
+        # result_cond = ' WHERE z.jaro_score::DOUBLE PRECISION > _str.jaro_score::DOUBLE PRECISION'
+        #
+        # for it in b_str_conditions:
+        #     log_to_file('cond #: %s' % b_str_conditions.index(it))
+        #     b_str_cond = ' WHERE ' + it
+        #     if EMBED:
+        #         import IPython as I
+        #         I.embed_kernel()
+        #     run_iter()
 
     elif iter_round=='third':
         a_prefix,a_suffix = '',',ts_div_line'
